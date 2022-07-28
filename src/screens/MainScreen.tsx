0fcff77a11d906231532095059bded59c680ccb2
wjddnjs7895 <53262722+wjddnjs7895@users.noreply.github.com>
2022-07-28 09:45:24 +0900

    ADD MainScreen

---
+++ src/screens/MainScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import MainButton from '../components/Button/MainButton';
+
+const MainScreen = () => {
+  return (
+    <MainButton>
+      <Text>MainScreen</Text>
+    </MainButton>
+  );
+};
+
+export default MainScreen;
