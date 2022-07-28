14ac2f78ee981313b010de7f26cbdc1b9446bb1c
wjddnjs7895 <53262722+wjddnjs7895@users.noreply.github.com>
2022-07-28 09:52:47 +0900

    Update MainScreen to Navigation

--- src/navigation/NavigationRoute.tsx
+++ src/navigation/NavigationRoute.tsx
@@ -5,9 +5,10 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import CalendarScreen from '../screens/CalendarScreen';
 import SearchScreen from '../screens/SearchScreen';
-import BubbleScreen from '../screens/BubbleScreen';
+//import BubbleScreen from '../screens/BubbleScreen';
 import NoteListScreen from '../screens/NoteListScreen';
 import UserScreen from '../screens/UserScreen';
+import MainScreen from '../screens/MainScreen';
 
 const BottomTab = createBottomTabNavigator();
 function BottomTabNavigator() {
@@ -15,7 +16,7 @@ function BottomTabNavigator() {
     <BottomTab.Navigator initialRouteName="CalendarScreen">
       <BottomTab.Screen name="CalendarScreen" component={CalendarScreen} options={{ title: 'Calendar' }} />
       <BottomTab.Screen name="SearchScreen" component={SearchScreen} options={{ title: 'Search' }} />
-      <BottomTab.Screen name="BubbleScreen" component={BubbleScreen} options={{ title: 'Bubble' }} />
+      <BottomTab.Screen name="MainScreen" component={MainScreen} options={{ title: 'Main' }} />
       <BottomTab.Screen name="NoteListScreen" component={NoteListScreen} options={{ title: 'NoteList' }} />
       <BottomTab.Screen name="UserScreen" component={UserScreen} options={{ title: 'User' }} />
     </BottomTab.Navigator>
