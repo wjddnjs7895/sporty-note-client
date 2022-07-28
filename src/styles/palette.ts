f560d35125c6cd62f9dd2da4a79ad8c64c0ffea2
wjddnjs7895 <53262722+wjddnjs7895@users.noreply.github.com>
2022-07-28 09:44:57 +0900

    ADD palette

---
+++ src/styles/palette.ts
@@ -0,0 +1,17 @@
+type PaletteType = {
+  [index: string]: string;
+};
+
+export const palette: PaletteType = {
+  red: '#FF0000',
+  orange: '#FFA500',
+  yellow: '#FFFF00',
+  green: '#008000',
+  blue: '#0000FF',
+  indigo: '#4B0082',
+  violet: '#EE82EE',
+  white: '#FFFFFF',
+  black: '#000000',
+};
+
+export type PaletteKeyTypes = keyof typeof palette;
