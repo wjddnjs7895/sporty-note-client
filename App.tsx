732ab65f8fadc0f087e3de4f5a8c25bc5d1fb26e
wjddnjs7895 <53262722+wjddnjs7895@users.noreply.github.com>
2022-08-04 17:55:11 +0900

    Add react settings

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Navigation from './src/navigation/NavigationRoute';
 
-const App = () => {
+function App() {
   return <Navigation />;
-};
+}
 
 export default App;
